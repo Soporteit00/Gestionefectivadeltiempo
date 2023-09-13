c0aeb78c940f6c1afe31e9bcfd7005a4b6d6e84f
Aprendiz Sistemas <AprendizSistemas@amar.local>
2023-09-13 09:45:12 -0500

    Gestion efectiva del tiempo

---
+++ Sections/Page25/Page25.js
@@ -0,0 +1,29 @@
+const audio = document.getElementById("audio");
+
+function reproducirAudio() {
+  audio.play();
+}
+
+function pausarAudio() {
+  audio.pause();
+}
+
+const audio2 = document.getElementById("audio2");
+
+function reproducirAudio2() {
+  audio2.play();
+}
+
+function pausarAudio2() {
+  audio2.pause();
+}
+
+const audio3 = document.getElementById("audio3");
+
+function reproducirAudio3() {
+  audio3.play();
+}
+
+function pausarAudio3() {
+  audio3.pause();
+}
